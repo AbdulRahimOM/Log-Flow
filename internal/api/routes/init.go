f4083c3b684261e85cae3ec8dcdb1195d52e497d
AbdulRahimOM <abdulrahimovingal@gmail.com>
2025-03-26 19:56:28 +0530

    misc

--- internal/api/routes/init.go
+++ internal/api/routes/init.go
@@ -6,29 +6,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// func MountRoutes(
-// 	app *fiber.App,
-// 	rabbitMQLogQueue queue.LogQueueSender,
-// 	liveProgressMessenger queue.ProgressMessenger,
-// ) {
-// 	supaBase := storage.NewSupabaseStorage(config.Env.SupaBaseURL, config.Env.SupaBaseKey, config.Env.SupaBaseBucket)
-// 	database := db.GetDB()
-
-// 	httpHandler := handler.NewHttpHandler(rabbitMQLogQueue, supaBase, database)
-// 	websocketManager := handler.NewWebSocketManager(liveProgressMessenger)
-
-// 	// health check
-// 	app.Get("/health", httpHandler.HealthCheck)
-
-// 	//http routes
-// 	mountAuthRoutes(app, httpHandler)
-// 	mountLogRoutes(app, httpHandler)
-
-// 	//websocket routes
-// 	mountWebSocketRoutes(app, websocketManager)
-
-// }
-
 func MountRoutes(
 	app *fiber.App,
 	httpHandler *handler.HttpHandler,
