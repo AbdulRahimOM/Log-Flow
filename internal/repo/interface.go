a08b3cb105dd9e9549bee060ff7e4ad1aad7767a
Abdul Rahim O M <abdulrahimovingal@gmail.com>
2025-03-29 00:12:21 +0530

    :memo: README (#14)

--- internal/repo/interface.go
+++
@@ -1,5 +0,0 @@
-package repo
-
-type Repository interface {
-	SaveLogMetricsToDB(jobID string, totalLogs, errorCount, warnCount, infoCount, uniqueIPs int) error
-}
--- internal/repo/log.go
+++
@@ -1,5 +0,0 @@
-package repo
-
-func (r *repo) SaveLogMetricsToDB(jobID string, totalLogs, errorCount, warnCount, infoCount, uniqueIPs int) error {
-	return nil
-}
