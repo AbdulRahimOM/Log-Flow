5875bc86e3b078a2bce9d7f599eb882ab859e72a
AbdulRahimOM <abdulrahimovingal@gmail.com>
2025-03-28 17:56:04 +0530

    Docker, Docker compose

--- internal/infrastructure/config/config.go
+++ internal/infrastructure/config/config.go
@@ -1,17 +1,18 @@
 package config
 
 type AppSettings struct {
-	Port             string `mapstructure:"PORT"`
-	LogLevel         string `mapstructure:"LOG_LEVEL"`
-	GeneralRateLimit int    `mapstructure:"GENERAL_RATE_LIMIT"`
-	AuthEndpointsRateLimit   int    `mapstructure:"AUTH_ENDPOINTS_RATE_LIMIT"`
+	Port                   string `mapstructure:"PORT"`
+	LogLevel               string `mapstructure:"LOG_LEVEL"`
+	GeneralRateLimit       int    `mapstructure:"GENERAL_RATE_LIMIT"`
+	AuthEndpointsRateLimit int    `mapstructure:"AUTH_ENDPOINTS_RATE_LIMIT"`
 }
 
 type SupaBase struct {
-	SupaBaseURL       string `mapstructure:"SUPABASE_URL"`
-	SupaBaseKey       string `mapstructure:"SUPABASE_KEY"`
-	SupaBaseBucket    string `mapstructure:"SUPABASE_BUCKET"`
-	SupaBaseJwtSecret string `mapstructure:"SUPABASE_JWT_SECRET_KEY"`
+	SupaBaseURL              string `mapstructure:"SUPABASE_URL"`
+	SupaBaseKey              string `mapstructure:"SUPABASE_KEY"`
+	SupaBaseBucket           string `mapstructure:"SUPABASE_BUCKET"`
+	SupaBaseJwtSecret        string `mapstructure:"SUPABASE_JWT_SECRET_KEY"`
+	SupaBaseProjectReference string `mapstructure:"SUPABASE_PROJECT_REFERENCE"`
 }
 
 type Postgres struct {
